b1087e301389f0eacc188caba8ba7534d3e55050
Einhornhool <einhornhool@mailbox.org>
2021-04-12 13:38:38 +0100

    Add function to announce type of trash when opening lid

--- talking-trash.py
+++ talking-trash.py
@@ -7,40 +7,53 @@ import os
 import random
 
 # Create a list of all available audio files in the directory 'audio'
-audiofiles = [os.path.join(r,fi) for r,d,f in os.walk('/home/pi/trash_talk/audio') for fi in f]
+audiofiles = [os.path.join(r,fi) for r,d,f in os.walk('/home/pi/trash_talk/audio/random') for fi in f]
 
 # Create a list of WaveObjects from the available audiofiles
 audio_obj = [sa.WaveObject.from_wave_file(f) for f in audiofiles]
 audio_len = len(audio_obj)
 
+biowaste_obj = sa.WaveObject.from_wave_file('audio/TTF_Biowaste.wav')
+
 accel = msa301.MSA301()
 accel.reset()
 accel.set_power_mode('normal')
 accel.enable_interrupt([
-    'y_active_interrupt',
+    'x_active_interrupt',
     'z_active_interrupt'])
 
-# Get base value (should be 0, but you never know)
-base_x, base_y, base_z = accel.get_measurements()
+open_val = 0.1
+close_val = 0.9
+last_z = close_val
 
-# When closing the lid, the last value will rarely be exactly 0. For this particular sensor it's always below 0.1, though.
-# May not apply to all sensors
-tolerance = 0.1
+opened = False
 
-last_z = base_z
+def lid_closed():
+    print("Lid closed, Curr: {0}".format(curr_z))
+    # Get random audio object to play
+    rand_obj = audio_obj[random.randint(0, audio_len-1)]
+    play_obj = rand_obj.play()
+    play_obj.wait_done()
+
+def lid_opened():
+    print("Lid opened, Curr: {0}".format(curr_z))
+    play_obj = biowaste_obj.play()
+    play_obj.wait_done()
+
 
 try:
     while (True):
         # Wait until movement is detected and get current measurements        
         accel.wait_for_interrupt('active_interrupt')
         curr_x, curr_y, curr_z = accel.get_measurements()
+        #print("X: {0}, Y: {1}, Z: {2}".format(curr_z, curr_y, curr_z))
        
-        if (last_z > curr_z) and (curr_z <= (base_z + tolerance)):
-            print("Lid closed, Curr: {0}".format(curr_z))
-            # Get random audio object to play
-            rand_obj = audio_obj[random.randint(0, audio_len-1)]
-            play_obj = rand_obj.play()
-            play_obj.wait_done()
+        if (last_z < curr_z) and (curr_z >= close_val) and opened:
+            lid_closed()
+            opened = False
+        if (last_z > curr_z) and (curr_z < open_val) and not opened:
+            lid_opened()
+            opened = True
 
         last_z = curr_z
 
